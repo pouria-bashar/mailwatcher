7aa668dd4fa44361b25af607404537f7dc69942a
Pouria <pouria.bashardoust@gmail.com>
2016-04-19 20:39:03 +1000

    Added MailWatcher dao

---
+++ src/com/pba/mailwatcher/dao/MailDao.java
@@ -0,0 +1,19 @@
+package com.pba.mailwatcher.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component("mailDao")
+public class MailDao {
+
+private NamedParameterJdbcTemplate jdbc;
+	
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
+}

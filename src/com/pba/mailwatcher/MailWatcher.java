78db05b770fe2b22e87844cbb2d47ab8a4f5bf13
Pouria <pouria.bashardoust@gmail.com>
2016-04-24 22:55:11 +1000

    Added comments

--- src/com/pba/mailwatcher/MailWatcher.java
+++ src/com/pba/mailwatcher/MailWatcher.java
@@ -1,34 +1,37 @@
 package com.pba.mailwatcher;
 
 import java.util.List;
-import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 import com.pba.mailwatcher.dao.MailWatcherDAO;
 import com.pba.mailwatcher.entities.Message;
 import com.pba.mailwatcher.mail.MailService;
 
+/**
+ * Checks database for messages to send and sends them via SMTP
+ * @author Pouria
+ */
 @Component
 public class MailWatcher implements Runnable {
 	private static final Logger logger = Logger.getLogger(MailWatcher.class.getName());
 	
+	/* Config object */
 	@Autowired
 	private MailwatcherConfig config;
 
+	/* Milawatcher database access */
 	@Autowired
 	private MailWatcherDAO mailWatcherDAO;
-
+	/* Mail service */
 	@Autowired
 	private MailService mailService;
 
 	@Override
 	public void run() {
 
+		/* get a list of messages to send */
 		List<Message> messages = mailWatcherDAO.getMessages(config.getMaxNumberOfMessages());
 		logger.log(Level.FINE, "Retrieved {0} messages", messages.size());
 
@@ -42,7 +45,6 @@ public class MailWatcher implements Runnable {
 		}
 		/* If there are not messages Poll */
 		try {
-			
 			
 			Thread.sleep(config.getMaxPollTime());
 		} catch (InterruptedException e) {

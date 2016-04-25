77bdb40dbde37add2b8d88ad7556ef7b0acfdba0
Pouria <pouria.bashardoust@gmail.com>
2016-04-25 17:45:24 +1000

    Added unit tests

--- src/test/java/com/pba/mailwatcher/dao/MailWatcherDAOImpTest.java
+++ src/test/java/com/pba/mailwatcher/dao/MailWatcherDAOImpTest.java
@@ -1,40 +1,42 @@
 package com.pba.mailwatcher.dao;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+
 import com.pba.mailwatcher.entities.Message;
+
 public class MailWatcherDAOImpTest {
 
-	
 	@Test
-	public void test_getMessages(){
-		
-		MailWatcherDAO mailWatcherDAO = new MailWatcherDAOImp(){
+	public void test_getMessages() {
+		final int maxNumberOfMessages = 10;
+		MailWatcherDAO mailWatcherDAO = new MailWatcherDAOImp() {
 			@Override
 			public JdbcTemplate getJdbcTemplate() {
-				return new JdbcTemplate(){
+				return new JdbcTemplate() {
 					@Override
 					public <T> List<T> query(String sql, Object[] args, RowMapper<T> rowMapper)
 							throws DataAccessException {
-						// TODO Auto-generated method stub
-						return null;
+						assertEquals(1, args.length);
+						assertEquals(maxNumberOfMessages, args[0]);
+						List messages = new ArrayList<>();
+						messages.add(new Message());
+						
+						return messages;
 					}
 				};
-				
+
 			}
-			
 		};
-		
-		
-		List<Message> messages = mailWatcherDAO.getMessages(10);
-		assertNull(messages);
-		
-		
+
+		List<Message> messages = mailWatcherDAO.getMessages(maxNumberOfMessages);
+		assertNotNull(messages);
 	}
 }

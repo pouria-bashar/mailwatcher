eefe65075484138e8a98d965bb5406a826876406
Pouria <pouria.bashardoust@gmail.com>
2016-04-25 15:34:35 +1000

    Added unit test

--- src/test/java/com/pba/mailwatcher/dao/MailWatcherDAOImpTest.java
+++ src/test/java/com/pba/mailwatcher/dao/MailWatcherDAOImpTest.java
@@ -1,27 +1,16 @@
 package com.pba.mailwatcher.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
-
 import com.pba.mailwatcher.entities.Message;
 public class MailWatcherDAOImpTest {
 
-	private boolean setDataSourceCalled;
-	
-	@Before
-	public void setup(){
-		setDataSourceCalled = false;
-	}
 	
 	@Test
 	public void test_getMessages(){
@@ -31,7 +20,7 @@ public class MailWatcherDAOImpTest {
 			public JdbcTemplate getJdbcTemplate() {
 				return new JdbcTemplate(){
 					@Override
-					public <T> List<T> query(PreparedStatementCreator psc, RowMapper<T> rowMapper)
+					public <T> List<T> query(String sql, Object[] args, RowMapper<T> rowMapper)
 							throws DataAccessException {
 						// TODO Auto-generated method stub
 						return null;
@@ -39,15 +28,13 @@ public class MailWatcherDAOImpTest {
 				};
 				
 			}
-			@Override
-			public void setDataSource(DataSource dataSource) {	
-				setDataSourceCalled = true;
-			}
+			
 		};
-		assertTrue(setDataSourceCalled);
+		
 		
 		List<Message> messages = mailWatcherDAO.getMessages(10);
+		assertNull(messages);
 		
-		assertEquals(0, messages.size());
+		
 	}
 }

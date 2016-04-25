1a8f808a8049e78571a49b8dd455a74e28d96617
Pouria <pouria.bashardoust@gmail.com>
2016-04-25 12:49:43 +1000

    Modified the directory setup

---
+++ src/test/java/com/pba/mailwatcher/MailwatcherTest.java
@@ -0,0 +1,12 @@
+package com.pba.mailwatcher;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class MailwatcherTest {
+
+	@Test
+	public void test(){
+		System.out.println("HELooooooo");
+		assertTrue(true);
+	}
+}
